525c9fccebcddf066e72f97378bee24d6e41fdc2
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-03-13 22:26:07 +0000

    Adding frontend base file structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AdminComponent } from './admin/admin/admin.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent}
--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AdminComponent } from './admin.component';
-
 @NgModule({
-  declarations: [ AdminComponent ],
+  declarations: [],
   imports: [
     CommonModule
   ]
 })
-export class AdminModule { }
+export class CoreModule { }
