fb8081deac50482fc3d6cb27a83b711f21aaf56c
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-05-07 14:59:54 +0100

    Fix guard disfunction

--- frontend/src/app/core/services/auth.guard.ts
+++ frontend/src/app/core/services/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 
 import { UserService } from './user.service';
+import { take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,14 @@ export class AuthGuard implements CanLoad {
   constructor(private userService: UserService, private router: Router) {}
 
   canLoad() {
-    let canLoad: boolean;
-    this.userService.isAuthenticated.subscribe(b => canLoad = b);
-    
-    if(!canLoad) {
-      console.log('Not logged in. Access denied.');
-      this.router.navigateByUrl('login');
-    }
-    
-    return canLoad;
+    return this.userService.isAuthenticated.pipe(
+      take(1), // to send an onComplete message in the observer. Otherwise, the guard waits for it
+      tap(isAuth => {
+        if (!isAuth) {
+          console.log('Not logged in. Access denied.');
+          this.router.navigateByUrl('login');
+        }
+      })
+    );
   }
 }
