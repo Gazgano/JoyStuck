a9337611b8c00cc70c49247ea8dc3f14a1dbd83b
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-05-07 18:14:15 +0100

    Centralize external modules in shared module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SharedModule } from './shared/shared.module';
 
 @ NgModule({
   declarations: [
@@ -17,11 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     AppRoutingModule,
     CoreModule,
-    FormsModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatProgressSpinnerModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { 
+  MatButtonModule, 
+  MatFormFieldModule, 
+  MatIconModule, 
+  MatInputModule, 
+  MatMenuModule, 
+  MatProgressSpinnerModule ,
+  MatToolbarModule
+} from '@angular/material';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material';
 
 import { FooterComponent } from './shell/footer/footer.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { ShellComponent } from './shell/shell.component';
+
 
 @NgModule({
   declarations: [
@@ -15,18 +25,26 @@ import { ShellComponent } from './shell/shell.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatMenuModule,
+    MatProgressSpinnerModule,
     MatToolbarModule,
     RouterModule
   ],
   exports: [
     CommonModule,
+    FormsModule,
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatMenuModule,
     MatToolbarModule,
+    MatProgressSpinnerModule,
     ShellComponent
   ]
 })
