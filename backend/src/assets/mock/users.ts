54f5e3d9ab563888c32ccfd6adf7bb91154265b4
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-07-14 16:11:24 +0100

    More users in mock

--- backend/src/assets/mock/users.ts
+++ backend/src/assets/mock/users.ts
@@ -10,6 +10,30 @@ const users = [
         username: 'Gazgano',
         profileImageFileName: 'gazgano-picture-64px.png',
         profileImageSrcUrl: ''
+    },
+    {
+        id: 3,
+        username: 'Imbibed',
+        profileImageFileName: 'gazgano-picture-64px.png',
+        profileImageSrcUrl: ''
+    },
+    {
+        id: 4,
+        username: 'Nini',
+        profileImageFileName: 'gazgano-picture-64px.png',
+        profileImageSrcUrl: ''
+    },
+    {
+        id: 5,
+        username: 'ClemDOT',
+        profileImageFileName: 'gazgano-picture-64px.png',
+        profileImageSrcUrl: ''
+    },
+    {
+        id: 6,
+        username: 'Zetsu',
+        profileImageFileName: 'gazgano-picture-64px.png',
+        profileImageSrcUrl: ''
     }
 ];
 
