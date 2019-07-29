32db5fc4370637e12b9028cbc67410165ba19899
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-07-29 17:11:55 +0100

    Firebase installation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@app/core/services/auth.service';
+import * as firebase from 'firebase/app';
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBGEtojvCF9mRTVFcPvbFDx6A56VPCUBw4',
+  authDomain: 'joystuck.firebaseapp.com',
+  databaseURL: 'https://joystuck.firebaseio.com',
+  projectId: 'joystuck',
+  storageBucket: '',
+  messagingSenderId: '612325519517',
+  appId: '1:612325519517:web:e0a724f26bad259b'
+};
 
 @Component({
   selector: 'app-root',
@@ -11,6 +23,7 @@ export class AppComponent implements OnInit {
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    firebase.initializeApp(firebaseConfig);
     this.authService.populate();
   }
 }
