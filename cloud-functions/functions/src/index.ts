69f30567a8f9eec8e1e68fa511486219f9b067f4
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-08-23 17:34:46 +0100

    Adding testing cloud-functions

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,8 +1,60 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const moment = require('moment');
+import * as firestore from '@google-cloud/firestore';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-export const helloWorld = functions.https.onRequest((request: any, response: any) => {
-  response.send('Hello from Firebase!');
+const getPostById = function(postId: string): Promise<firestore.DocumentData | undefined> {
+  return new Promise((resolve, reject) => {
+    db.collection('posts').doc(postId).get()
+    .then((docSnapshot: firestore.DocumentSnapshot) => {
+        if(docSnapshot.exists) {
+          resolve(docSnapshot.data());
+        } else {
+          reject(`No post document found.`);
+        }
+    })
+    .catch(err =>
+      reject('Error getting post document')
+    )
+  })
+}
+
+const getFirstComment = function(): Promise<firestore.DocumentReference> {
+  return new Promise((resolve, reject) => {
+    db.collection('comments').get()
+    .then((snapshot: firestore.QuerySnapshot) => {
+        if(snapshot.docs && snapshot.docs.length > 0) {
+          resolve(snapshot.docs[0].data().post_id);
+        } else {
+          reject(`No comment document found.`);
+        }
+    })
+    .catch(err => {
+      reject('Error getting comments');
+    });
+  });
+}
+
+export const getTest = functions.https.onRequest((req, res) => {
+  getFirstComment()
+  .then(commentRef => getPostById(commentRef.id))
+  .then(doc => res.status(200).send(JSON.stringify(doc)))
+  .catch(err => res.status(500).send(err));
+});
+
+export const addTest = functions.https.onRequest(async (req, res) => {
+  db.collection('comments').add({
+    post_id: 'posts/PdwgjciuD0T055LqeqtL',
+    authorName: 'Gazgano',
+    timestamp: firestore.Timestamp.now(),
+    content: 'Occaecat minim laborum',
+    likesCount: 0
+  }).then((ref: firestore.DocumentReference) => 
+    res.send(`Object written at ${ref.id}`)
+  ).catch(err =>
+    res.status(500).send(err)
+  );
 });
