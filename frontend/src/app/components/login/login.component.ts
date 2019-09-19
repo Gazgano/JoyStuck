0b3e4ec930d239b3c935b430f1d80484c5e877bd
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-09-19 14:15:23 +0100

    Designing profile page with file type input

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -56,7 +56,7 @@ export class LoginComponent implements OnInit, AfterContentInit {
   openSignupDialog() {
     this.matDialog.open(SignupDialogComponent, { 
       width: '420px',
-      height: '480px',
+      height: '420px',
       data: { email: this.emailFormControl.value } 
     });
   }
