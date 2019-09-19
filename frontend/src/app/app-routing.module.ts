80e83a39385deee73e13d0c939811e443ee488c4
Aurélien Rasse <aurelien.rasse28@gmail.com>
2019-09-19 10:50:26 +0100

    Moving AuthGuard in a new Guards folder

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from '@app/core/services/auth.guard';
+import { AuthGuard } from '@app/core/guards/auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
